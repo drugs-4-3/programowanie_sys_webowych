cb7f8da0502206f542ab96bdeb1cde4db7399afd
Piotrek Neumann <piotr.neumann@mail.com>
2017-11-14 13:12:05 +0100

    DOM zad 1,2,3

---
+++ function1.js
@@ -0,0 +1,17 @@
+function domZad1() {
+  var elem = document.createElement("li")
+  var elem1 = document.createElement("li")
+  var li = document.createTextNode("Woda gazowana")
+  var li1 = document.createTextNode("Coca Cola")
+  elem.appendChild(li);
+  elem1.appendChild(li1);
+
+  document.getElementById("myList").appendChild(elem);
+  var list = document.getElementById("myList");
+
+  list.insertBefore(elem1, list.childNodes[0]);
+  list.removeChild(list.childNodes[5]);
+
+  var parent = document.getElementById("container").parentNode.nodeName;
+  document.getElementById("name").innerHTML = parent;
+}
---
+++ js/function3.js
@@ -0,0 +1,9 @@
+function chColor() {
+  console.log("hahahahh");
+
+  var color = document.getElementById("colorValue").value;
+  console.log(color);
+  document.body.style.backgroundColor = color;
+  document.body.style.color = "red";
+  document.body.style.fontFamily = "Impact,Charcoal,sans-serif";
+}
